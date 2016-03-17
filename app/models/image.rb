af52cf8dc2314c18bf2385a6c3324e3d3061f2c9
phani05353 <phani05353@gmail.com>
2016-03-17 15:27:05 +0000

    commit last but three

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,14 +1,9 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
-  validate :picture_size
 
 
-  private
 
-  def picture_size
-    if picture.size >100.megabytes
-      error.add(:picture,"should be less than 5 MB" )
-    end
-  end
+
+
 end
